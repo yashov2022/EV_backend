6518c1620dacd912896ad0f46e4ba6eb485bd749
Yashovardhan A <yashrailfansbc@gmail.com>
2026-06-23 16:47:01 +0530

    add

--- src/main/java/com/ev/ev_project/config/SecurityConfig.java
+++ src/main/java/com/ev/ev_project/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.ev.ev_project.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,6 +36,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                HttpMethod.OPTIONS,
+                                "/**"
+                        ).permitAll()
 
                         .requestMatchers(
                                 "/auth/**"
