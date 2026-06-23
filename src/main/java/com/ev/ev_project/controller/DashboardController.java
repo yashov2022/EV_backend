e635ad5aad1b5b6f3dea5ad0287efdc96f810097
Yashovardhan A <yashrailfansbc@gmail.com>
2026-06-23 20:10:08 +0530

    add

--- src/main/java/com/ev/ev_project/controller/DashboardController.java
+++ src/main/java/com/ev/ev_project/controller/DashboardController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dashboard")
-@CrossOrigin(origins = "http://localhost:5173")
 public class DashboardController {
     @Autowired
     private DashboardService service;
